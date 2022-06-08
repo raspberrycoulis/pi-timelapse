b48785609e34ab2c563391ddfc30e3223f8d6ccc
Wesley Archer <6330169+raspberrycoulis@users.noreply.github.com>
2022-06-08 17:08:40 +0100

    Tweaked output messages

--- timelapse.py
+++ timelapse.py
@@ -3,11 +3,11 @@ from os import system
 import datetime
 from time import sleep
 
-timelapse = 10 #set this to the number of minutes you wish to run your timelapse camera
+timelapse = 1 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 1 #number of seconds delay between each photo taken
 fps = 30 #frames per second timelapse video
 numphotos = int((timelapse*60)/secondsinterval) #number of photos to take
-print("number of photos to take = ", numphotos)
+print("Taking ", numphotos, " photos.")
 
 dateraw= datetime.datetime.now()
 datetimeformat = dateraw.strftime("%d %m %Y at %H:%M")
@@ -25,5 +25,4 @@ print("Finished taking photos.")
 print("Now creating timelapse video. Please wait...")
 
 system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
-#system('rm /home/pi/Pictures/*.jpg')
 print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
